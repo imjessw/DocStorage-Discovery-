e8be6cfcbf5d313d6055aafb9aeb917373a423d2
Jessica Waite <imjessw@gmail.com>
2016-03-17 16:35:21 -0400

    adding images

--- docStorage/client/templates/myImages/myImages.js
+++ docStorage/client/templates/myImages/myImages.js
@@ -1,11 +1,40 @@
 
-Template.myImages.events({
-  'change .myFileInput': function(event, template) {
-    var files = event.target.files;
-    for (var i = 0, ln = files.length; i < ln; i++) {
-      Images.insert(files[i], function (err, fileObj) {
-        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
-      });
+Template.myImages.helpers({
+    'files':function(){
+        return Images.find({},{sort:{'uploadedAt':-1}}).fetch()
+    },
+    'theImageUrl':function(){
+        return Images.findOne(this._id).url();
+    },
+    'isImage':function(){
+        return Images.findOne(this._id).isImage()
+    },
+    'raw':function(){
+        return EJSON.stringify(this,{'indent':true})
     }
-  }
 });
+
+Template.myImages.events({
+    'change .myFileInput': function(event, template) {
+        var files = event.target.files;
+        for (var i = 0, ln = files.length; i < ln; i++) {
+            Images.insert(files[i], function (err, fileObj) {
+                console.log('fileObj',fileObj);
+                // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+            });
+        }
+    }
+});
+
+Template.myImages.onCreated(function () {
+    //add your statement here
+});
+
+Template.myImages.onRendered(function () {
+    //add your statement here
+});
+
+Template.myImages.onDestroyed(function () {
+    //add your statement here
+});
+
--- docStorage/server/server.js
+++ docStorage/server/server.js
@@ -1,7 +1,3 @@
-Images = new FS.Collection("images", {
-  stores: [new FS.Store.FileSystem("images", {path: "~/uploads"})]
-});
-
 Images.allow({
   'insert': function () {
     // add custom authentication code here
