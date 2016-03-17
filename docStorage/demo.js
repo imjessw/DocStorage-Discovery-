6f177c9719a5f7aead941e017c5dee1d2d02a181
Jessica Waite <imjessw@gmail.com>
2016-03-17 15:57:21 -0400

    added buttons/ bootstrap/ fixing Images error

---
+++ docStorage/demo.js
@@ -0,0 +1,11 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
