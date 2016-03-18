05fbd618b4a265b6b9b9a03e5fd00061853235e9
Jessica Waite <imjessw@gmail.com>
2016-03-18 15:08:00 -0400

    updated js for myDocs

--- docStorage/client/templates/myDocs/myDocs.js
+++ docStorage/client/templates/myDocs/myDocs.js
@@ -1,2 +1,27 @@
 
+Template.myDocs.helpers({
+    'files':function(){
+        return Files.find({},{sort:{'uploadedAt':-1}}).fetch()
+    },
+    'theImageUrl':function(){
+        return Files.findOne(this._id).url();
+    },
+    'isImage':function(){
+        return Files.findOne(this._id).isImage()
+    },
+    'raw':function(){
+        return EJSON.stringify(this,{'indent':true})
+    }
+});
 
+Template.myDocs.events({
+    'change .myFileInput': function(event, template) {
+        var files = event.target.files;
+        for (var i = 0, ln = files.length; i < ln; i++) {
+            Files.insert(files[i], function (err, fileObj) {
+                console.log('fileObj',fileObj);
+                // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+            });
+        }
+    }
+});
